4c5d4bb7b160d1e8025ac722de6d29cde26d52b7
lakshith <lakshith.k.nishshanke@gmail.com>
2024-05-22 13:08:50 +0530

    render attention for given data

--- python/inspectus/txt_attention.py
+++ python/inspectus/txt_attention.py
@@ -1,3 +1,8 @@
+import json
+from typing import List
+import numpy as np
+
+
 JS_CSS_ADDED = False
 
 
@@ -7,7 +12,7 @@ def init_inline_viz():
     global JS_CSS_ADDED
 
     if not JS_CSS_ADDED:
-        html += '''<script src="https://cdn.statically.io/gist/lakshith-403/6c40cc4e37d07676f421108d9d0109a6/raw/3570ac6da38a5d7250c6b6aee3e18bcf70b71d0f/labml_attn.js"></script>'''
+        html += '''<script src="https://cdn.statically.io/gist/lakshith-403/6c40cc4e37d07676f421108d9d0109a6/raw/bf7eb154f409b91a0c9afdc66dfe2cf2e8283347/labml_attn_without_sample.js"></script>'''
         html += '''<link rel="stylesheet" href="https://cdn.statically.io/gist/lakshith-403/81b16b30a7f2c853a536f3d3975a7693/raw/aaee9494e96c5370b27821e114562d2d8e64ce39/labml_attn.css">'''
         JS_CSS_ADDED = True
 
@@ -16,7 +21,22 @@ def init_inline_viz():
     display(HTML(html))
 
 
-def text_attention():
+def text_attention(attn_mat: np.ndarray, src_tokens: List['str'] = None, tgt_tokens: List['str'] = None):
+    """
+        This function visualizes the attention matrix of a transformer model in a Jupyter notebook.
+
+        Parameters:
+        attn_mat (np.ndarray): A 4D numpy array representing the attention matrix.
+                               The dimensions are [layers, heads, n_src, n_tgt].
+        src_tokens (List[str], optional): A list of source tokens. Defaults to a list of 'A' repeated n_src times.
+        tgt_tokens (List[str], optional): A list of target tokens. Defaults to a list of 'T' repeated n_tgt times.
+
+        Raises:
+        AssertionError: If the length of src_tokens or tgt_tokens does not match n_src or n_tgt respectively.
+
+        Returns:
+        None
+        """
     html = ''
 
     from uuid import uuid1
@@ -24,9 +44,31 @@ def text_attention():
 
     html += f'<div id="{elem_id}"></div>'
 
+    layers, heads, n_src, n_tgt = attn_mat.shape
+
+    if src_tokens is None:
+        src_tokens = ['A'] * n_src
+    if tgt_tokens is None:
+        tgt_tokens = ['T'] * n_tgt
+
+    assert len(src_tokens) == n_src
+    assert len(tgt_tokens) == n_tgt
+
+    data = []
+    for layer in range(layers):
+        for head in range(heads):
+            data.append({'values': attn_mat[layer, head].tolist(),
+                         'info': {'layer': layer, 'head': head}})
+
+    res = json.dumps({
+        'attention': data,
+        'src_tokens': src_tokens,
+        'tgt_tokens': tgt_tokens,
+    })
+
     script = ''
     script += '<script>'
-    script += f"window.chartsEmbed('{elem_id}')"
+    script += f"window.chartsEmbed('{elem_id}',{res})"
     script += '</script>'
 
     from IPython.display import display, HTML
--- typescript/src/charts.ts
+++ typescript/src/charts.ts
@@ -1,58 +1,18 @@
-import {Controller} from "./controller";
-import {sampleAttentions, sampleTokens} from "./sample_data";
+import {Controller} from "./controller"
+// import {sampleAttentions, sampleTokens} from "./sample_data"
 
-// document
-//     .addEventListener('DOMContentLoaded', () => {
-//         // test()
-//         test_server()
-//     })
-
-// @ts-ignore
-window.chartsEmbed=function(elemId: string){
-    let chart = new Controller([{name: 'layer', isMulti: true}, {name: 'head', isMulti: true}],
-        sampleAttentions,
-        {tokens: sampleTokens},
-        {tokens: sampleTokens})
-    document.getElementById(elemId).appendChild(chart.render())
-}
-
-function test() {
-    let src = ['I', ' ', 'went', ' ', 'home']
-    let attention = [
-        [0, 0, .7, 0, .3],
-        [0, 0, 0, 1, 0],
-        [.5, 0, 0, 0, .5],
-        [0, 1, 0, 0, 0],
-        [.2, 0, .8, 0, 0],
-    ]
-    // let chart = new Controller([{name: 'layer', isMulti: true}],
-    //     [{values: attention, info: {'layer': '0'}}],
-    //     {tokens: src},
-    //     {tokens: src})
-
-    let chart = new Controller([{name: 'layer', isMulti: true}, {name: 'head', isMulti: true}],
-        sampleAttentions,
-        {tokens: sampleTokens},
-        {tokens: sampleTokens})
-    document.body.appendChild(chart.render())
-}
-
-
-function onData(e: ProgressEvent) {
-    let req = <XMLHttpRequest>e.target
-    let data = JSON.parse(req.responseText)
-
+window["chartsEmbed"]=function(elemId: string, data: any) {
     let chart = new Controller([{name: 'layer', isMulti: true}, {name: 'head', isMulti: true}],
         data.attention,
         {tokens: data.src_tokens},
         {tokens: data.tgt_tokens})
-    document.body.appendChild(chart.render())
-
+    document.getElementById(elemId).appendChild(chart.render())
 }
 
-function test_server() {
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", onData);
-    oReq.open("GET", "data");
-    oReq.send();
-}
+// function test() {
+//     let chart = new Controller([{name: 'layer', isMulti: true}, {name: 'head', isMulti: true}],
+//         sampleAttentions,
+//         {tokens: sampleTokens},
+//         {tokens: sampleTokens})
+//     document.body.appendChild(chart.render())
+// }
